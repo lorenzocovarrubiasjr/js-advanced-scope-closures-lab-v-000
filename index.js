5166047105965661eaa81531fdc8e51b762f99eb
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-27 02:11:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function produceDrivingRange(range) {
+  function (trip) {
+    return trip < range ? true : false;
+  }
+};
+
+function produceTipCalculator() {
+  
+};
+
+function createDriver() {
+  
+};
