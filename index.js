097410b7b918c8525c07f6216edd4d67c0e3ca7a
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-27 02:20:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(blockRange) {
   return function (tripStart, tripEnd) {
-    return (parseInt(tripEnd) - parseInt(tripStart)) < blockRange ? `within range by ${parseInt(tripEnd) - parseInt(tripStart)}` : false;
+    return ((parseInt(tripEnd) - parseInt(tripStart)) < blockRange) ? `within range by ${parseInt(tripEnd) - parseInt(tripStart)}` : false;
   }
 };
 
